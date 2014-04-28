0eee291df631d93bdca963ca5db66593772a56f9
Takumi MIURA <mitaku1104@gmail.com>
2014-04-29 01:18:04 +0900

    Modify app

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ Bundler.require
 require "sinatra/config_file"
 require "json"
 
+if ENV["REDISTOGO_URL"] != nil
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  redis = Redis.new(host: "127.0.0.1", port: "6379")
+end
+
+Redis.current = redis
+
 class Pizza
   include Redis::Objects
 
